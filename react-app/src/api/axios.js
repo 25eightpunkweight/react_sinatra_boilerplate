74bba95ebb47dbee375cc343c67284d34e5a4faa
David Lucero <dvlucero85@gmail.com>
2025-07-30 11:45:21 +0800

    axios adjustments specific to vite

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backend_host = import.meta.env.BACKEND_HOST || 'localhost';
+const backend_host = import.meta.env.BACKEND_HOST;
 
 const api = axios.create({
     baseURL: `http://${backend_host}:3000`
