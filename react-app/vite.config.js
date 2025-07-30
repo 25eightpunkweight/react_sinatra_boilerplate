5fabb595ea67bb8580a8f1aee23adcd8ad7ee596
David Lucero <dvlucero85@gmail.com>
2025-07-30 11:26:21 +0800

    deploy-ready environment variables

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const isProduction = process.env.NODE_ENV === "production";
-const allowedHosts = [""];
-
-if (isProduction && process.env.PUBLIC_FRONTEND_HOST) {
-    allowedHosts.push(process.env.PUBLIC_FRONTEND_HOST);
-}
-
 export default defineConfig({
  base: "/",
  plugins: [react()],
@@ -20,6 +13,6 @@ export default defineConfig({
   strictPort: true,
   host: true,
   origin: "http://0.0.0.0:1337",
-  allowedHosts,
+  allowedHosts: [process.env.PUBLIC_FRONTEND_HOST],
  },
 });
