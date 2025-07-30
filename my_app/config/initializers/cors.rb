46e9699729dcec02491ffdafac65b8f35a6315c3
David Lucero <dvlucero85@gmail.com>
2025-07-30 12:42:38 +0800

    rails cors config

--- my_app/config/initializers/cors.rb
+++ my_app/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "http://#{ENV['FRONTEND_HOST']}:1337"
-  
+      origins '*'
+
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
