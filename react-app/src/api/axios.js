bacb1382d4d6649937e2beb4abbc33d71c3ae344
David Lucero <dvlucero85@gmail.com>
2025-07-30 12:56:59 +0800

    more axios-related config

--- react-app/src/api/axios.js
+++ react-app/src/api/axios.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const backend_host = import.meta.env.VITE_BACKEND_HOST;
 const backend_protocol = backend_host === 'localhost' ? 'http' : 'https';
+const port = backend_host === 'localhost' ? ':3000' : '';
 
 const api = axios.create({
-    baseURL: `${backend_protocol}://${backend_host}:3000`
+    baseURL: `${backend_protocol}://${backend_host}${port}`
 });
 
 export default api;
